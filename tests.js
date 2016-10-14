ee806411b0c2db904bcd278e5615ea5473cab968
robocopkaka <robocopkaka@gmail.com>
2016-10-14 17:54:36 +0100

    Done 5 tests

--- tests.js
+++ tests.js
@@ -3,22 +3,32 @@
 var chai = require('chai');
 var assert = chai.assert;
 
-var lib = require('./lib/library.js');
+var lib = require('./lib/sumOfPrimes.js');
 
-describe("Test that constants are computed properly", function() {
-  it("should give 10, 4 for constants 2, 5 as a and n respectively", function() {
-    assert(
-      lib.compareCoefficients(lib.quadraticDerivative(2, 5), { a: 10, n: 4 })
-    );
+describe("Test that it returns the sum of prime numbers from 0 to n", function() {
+  it("should return a correct value for the sum of all prime numbers between 0 and 10", function(){
+    assert(lib.sumOfPrimes(10) === 17 )
   });
-  it("should give 2, 1 for constants 1, 2 as a and n respectively", function() {
-    assert(
-      lib.compareCoefficients(lib.quadraticDerivative(1, 2), { a: 2, n: 1 })
-    );
+
+  it("should return a correct value for the sum of all prime numbers between 0 and 20", function(){
+    assert(lib.sumOfPrimes(20) === 77 )
   });
-  it("should give 8, 1 for constants 4, 2 as a and n respectively", function() {
-    assert(
-      lib.compareCoefficients(lib.quadraticDerivative(4, 2), { a: 8, n: 1 })
-    );
+});
+
+describe("Test should return an error if it receives anything other than a number", function(){
+  it("should return an error message when a string is passed as a parameter", function(){
+    assert(lib.sumOfPrimes("5") === "Please pass a number")
+  })
+
+  it("should return an error message when an alphabet is passed as a parameter", function(){
+    assert(lib.sumOfPrimes(O) === "Please pass a number")
   });
+
+  it("should return an error message when a negative number is entered", function(){
+    assert(lib.sumOfPrimes(-1) === "Enter a number greater than 0")
+  });  
+});
+
+describe("", function(){
+
 });
