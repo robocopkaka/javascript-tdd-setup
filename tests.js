cac17488d92a01d41c806c679752bcb8f39392e6
robocopkaka <robocopkaka@gmail.com>
2016-10-14 18:07:52 +0100

    Added all 10 test cases

--- tests.js
+++ tests.js
@@ -13,6 +13,18 @@ describe("Test that it returns the sum of prime numbers from 0 to n", function()
   it("should return a correct value for the sum of all prime numbers between 0 and 20", function(){
     assert(lib.sumOfPrimes(20) === 77 )
   });
+
+  it("should return a correct value for the sum of all prime numbers betwen 0 and 50", function(){
+    assert(lib.sumOfPrimes(50) === 328)
+  });
+
+  it("should return a correct value for the sum of all prime numbers betwen 0 and 30", function(){
+    assert(lib.sumOfPrimes(30) === 129)
+  });
+
+  it("should return a correct value for the sum of all prime numbers betwen 0 and 100", function(){
+    assert(lib.sumOfPrimes(100) === 1060)
+  });
 });
 
 describe("Test should return an error if it receives anything other than a number", function(){
@@ -26,7 +38,15 @@ describe("Test should return an error if it receives anything other than a numbe
 
   it("should return an error message when a negative number is entered", function(){
     assert(lib.sumOfPrimes(-1) === "Enter a number greater than 0")
+  });
+
+  it("should return an error message when an array is passed as a parameter", function(){
+    assert(lib.sumOfPrimes([1,2]) === "Please, enter a number")
   });  
+});
+
+it("should return an error when a quotient is entered", function(){
+  assert(lib.sumOfPrimes(10.5) === "Please enter an integer number")
 });
 
 describe("", function(){
